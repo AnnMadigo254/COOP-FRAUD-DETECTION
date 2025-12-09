985d1f8e05c3fc8e9aca6c4c81f6788790100c4a
Ann Madigo <amadigo@co-opbank.co.ke>
2025-12-09 17:49:21 +0300

    added requirements.txt

--- fraud/urls.py
+++ fraud/urls.py
@@ -1,16 +1,8 @@
-from django.contrib import admin
+# fraud/urls.py
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#     path("predict/", views.PredictAPIView.as_view()),  
-#     path("predictrank/", views.PredictRanksAPIView.as_view())  
-# ]
-
-# from django.urls import path
-# from . import views
-
 urlpatterns = [
-    path('predict', views.FraudPredictionAPIView.as_view(), name='fraud-predict'),
-    path('health', views.ModelHealthAPIView.as_view(), name='fraud-health'),
+    path('predict/', views.FraudPredictionAPIView.as_view(), name='fraud-predict'),
+    path('health/', views.ModelHealthAPIView.as_view(), name='fraud-health'),
 ]
